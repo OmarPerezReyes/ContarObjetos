38f20a2de848a53878008f3a9b182fe65f958d4d
Omar Alejandro Pérez Reyes <2130073@upv.edu.mx>
2023-08-11 05:05:30 -0600

    Mostrar imagenes

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,65 @@
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
-import org.opencv.core.CvType;
-import org.opencv.core.Scalar;
+import org.opencv.core.MatOfByte;
+import org.opencv.core.Size;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+
+import javax.swing.*;
+import java.io.File;
 
 public class Main {
-    // Compulsory
-    static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
-
     public static void main(String[] args) {
-        System.out.println("Welcome to OpenCV " + Core.VERSION);
-        Mat m = new Mat(5, 10, CvType.CV_8UC1, new Scalar(0));
-        System.out.println("OpenCV Mat: " + m);
-        Mat mr1 = m.row(1);
-        mr1.setTo(new Scalar(1));
-        Mat mc5 = m.col(5);
-        mc5.setTo(new Scalar(5));
-        System.out.println("OpenCV Mat data:\n" + m.dump());
+        // Carga la biblioteca nativa de OpenCV
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+
+        // Ruta de la carpeta que contiene las imágenes
+        String folderPath = "C:\\Users\\perez\\IdeaProjects\\ContarObjetos\\assets\\photos";
+
+        // Crear un objeto File para representar la carpeta
+        File folder = new File(folderPath);
+
+        // Verificar si la carpeta existe
+        if (!folder.exists() || !folder.isDirectory()) {
+            System.out.println("La carpeta no existe o no es una carpeta válida.");
+            return;
+        }
+
+        // Obtener una lista de archivos en la carpeta
+        File[] files = folder.listFiles();
+
+        // Contador para controlar el número de imágenes procesadas
+        int count = 0;
+
+        // Recorrer los archivos y cargar las imágenes usando OpenCV
+        for (File file : files) {
+            if (file.isFile() && count < 5) { // Limitar a las primeras 5 imágenes
+                // Cargar la imagen utilizando OpenCV
+                Mat image = Imgcodecs.imread(file.getAbsolutePath());
+
+                // Verificar si la imagen se cargó correctamente
+                if (!image.empty()) {
+                    // Escalar la imagen a 640x480
+                    Imgproc.resize(image, image, new Size(640, 480));
+
+                    MatOfByte buffer = new MatOfByte();
+                    Imgcodecs.imencode(".jpg", image, buffer); // Convertir Mat a byte buffer
+
+                    ImageIcon icon = new ImageIcon(buffer.toArray()); // Convertir buffer a ImageIcon
+                    JLabel label = new JLabel(icon);
+
+                    // Crear una nueva ventana para cada imagen
+                    JFrame frame = new JFrame("Imagen: " + file.getName());
+                    frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                    frame.add(label);
+                    frame.pack();
+                    frame.setVisible(true);
+
+                    count++; // Incrementar el contador
+                } else {
+                    System.out.println("No se pudo cargar la imagen: " + file.getName());
+                }
+            }
+        }
     }
 }
