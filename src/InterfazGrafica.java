f938336769ec11909af85e285f16f453ea970d21
Omar Alejandro Pérez Reyes <2130073@upv.edu.mx>
2023-08-22 02:49:13 -0600

    Creación de interfaz gráfica con 3 botones
    Falta su implementación

---
+++ src/InterfazGrafica.java
@@ -0,0 +1,53 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class InterfazGrafica {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Detector de Objetos de Colores");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setPreferredSize(new Dimension(400, 200)); // Tamaño personalizado
+
+            JPanel panel = new JPanel();
+            panel.setLayout(new GridLayout(3, 1, 10, 10)); // Diseño en forma de cuadrícula
+
+            JButton preselectedButton = new JButton("Imagen preseleccionada");
+            JButton selectImageButton = new JButton("Seleccionar imagen");
+            JButton openCameraButton = new JButton("Abrir cámara");
+
+            panel.add(preselectedButton);
+            panel.add(selectImageButton);
+            panel.add(openCameraButton);
+
+            preselectedButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    // Acción cuando se presiona el botón "Imagen preseleccionada"
+                    JOptionPane.showMessageDialog(frame, "Imagen preseleccionada elegida");
+                }
+            });
+
+            selectImageButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    // Acción cuando se presiona el botón "Seleccionar imagen"
+                    JOptionPane.showMessageDialog(frame, "Seleccionar imagen elegida");
+                }
+            });
+
+            openCameraButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    // Acción cuando se presiona el botón "Abrir cámara"
+                    JOptionPane.showMessageDialog(frame, "Abrir cámara elegida");
+                }
+            });
+
+            frame.getContentPane().add(panel);
+            frame.pack();
+            frame.setVisible(true);
+        });
+    }
+}
