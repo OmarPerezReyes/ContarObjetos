172569344984421307e026f2c879d89b19878a92
Omar Alejandro Pérez Reyes <2130073@upv.edu.mx>
2023-08-22 01:52:28 -0600

    Corrección en división de métodos

--- src/Main.java
+++ src/Main.java
@@ -8,144 +8,146 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+    // Valores para segmentar el color azul en el espacio HSV
+    private static final double blueHueMin = 100;   // Rango mínimo de tono para el azul
+    private static final double blueHueMax = 130;   // Rango máximo de tono para el azul
+    private static final double cyanHueMin = 80;    // Rango mínimo de tono para el celeste
+    private static final double cyanHueMax = 100;   // Rango máximo de tono para el celeste
+    private static final double saturationMin = 100; // Mínima saturación (puede ajustarse)
+    private static final double saturationMax = 255; // Máxima saturación (puede ajustarse)
+    private static final double valueMin = 50;       // Valor mínimo (brillo) (puede ajustarse)
+    private static final double valueMax = 255;      // Valor máximo (brillo) (puede ajustarse)
+
     public static void main(String[] args) {
-        // Valores para segmentar el color azul en el espacio HSV
-        double blueHueMin = 100;    // Rango mínimo de tono para el azul
-        double blueHueMax = 130;    // Rango máximo de tono para el azul
 
-        double cyanHueMin = 80;    // Rango mínimo de tono para el celeste
-        double cyanHueMax = 100;    // Rango máximo de tono para el celeste
-
-        double saturationMin = 100; // Mínima saturación (puede ajustarse)
-        double saturationMax = 255; // Máxima saturación (puede ajustarse)
-
-        double valueMin = 50;   // Valor mínimo (brillo) (puede ajustarse)
-        double valueMax = 255;  // Valor máximo (brillo) (puede ajustarse)
-
-        // Carga la biblioteca nativa de OpenCV
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 
-        // Ruta de la carpeta que contiene las imágenes
         String folderPath = "C:\\Users\\perez\\IdeaProjects\\ContarObjetos\\assets\\photos";
+        processImages(folderPath);
+    }
 
-        // Crear un objeto File para representar la carpeta
+    private static void processImages(String folderPath) {
         File folder = new File(folderPath);
 
-        // Verificar si la carpeta existe
         if (!folder.exists() || !folder.isDirectory()) {
             System.out.println("La carpeta no existe o no es una carpeta válida.");
             return;
         }
 
-        // Obtener una lista de archivos en la carpeta
         File[] files = folder.listFiles();
-
-        // Contador para controlar el número de imágenes procesadas
         int count = 0;
 
-        // Recorrer los archivos y cargar las imágenes usando OpenCV
         for (File file : files) {
-            if (file.isFile() && count < 5) { // Limitar a las primeras 5 imágenes
-                try {
-                    // Cargar la imagen utilizando OpenCV
-                    Mat image = Imgcodecs.imread(file.getAbsolutePath());
-                    // Verificar si la imagen se cargó correctamente
-                    if (!image.empty()) {
-                        // Escalar la imagen a 640x480
-                        Imgproc.resize(image, image, new Size(640, 480));
-
-                        // Convertir la imagen a espacio de color HSV
-                        Mat hsvImage = new Mat();
-                        Imgproc.cvtColor(image, hsvImage, Imgproc.COLOR_BGR2HSV);
-
-                        // Definir rangos de colores en HSV para la segmentación de azules
-                        Scalar blueLowerBound = new Scalar(blueHueMin, saturationMin, valueMin);
-                        Scalar blueUpperBound = new Scalar(blueHueMax, saturationMax, valueMax);
-
-                        // Crear una máscara binaria para azules utilizando inRange
-                        Mat blueMask = new Mat();
-                        Core.inRange(hsvImage, blueLowerBound, blueUpperBound, blueMask);
-
-                        // Definir rangos de colores en HSV para la segmentación de celestes
-                        Scalar cyanLowerBound = new Scalar(cyanHueMin, saturationMin, valueMin);
-                        Scalar cyanUpperBound = new Scalar(cyanHueMax, saturationMax, valueMax);
-
-                        // Crear una máscara binaria para celestes utilizando inRange
-                        Mat cyanMask = new Mat();
-                        Core.inRange(hsvImage, cyanLowerBound, cyanUpperBound, cyanMask);
-
-                        // Combinar las máscaras para obtener elementos azules y celestes
-                        Mat combinedMask = new Mat();
-                        Core.add(blueMask, cyanMask, combinedMask);
-
-                        // Definir un área mínima para filtrar contornos pequeños
-                        double minContourArea = 100; // Puedes ajustar este valor
-                        // Encontrar los contornos en la máscara combinada
-                        List<MatOfPoint> contours = new ArrayList<>();
-                        Mat hierarchy = new Mat();
-                        Imgproc.findContours(combinedMask, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
-
-                        // Filtrar los contornos por área mínima y asignarlos a las listas de azules y celestes
-                        List<MatOfPoint> blueContours = new ArrayList<>();
-                        List<MatOfPoint> cyanContours = new ArrayList<>();
-                        for (MatOfPoint contour : contours) {
-                            double contourArea = Imgproc.contourArea(contour);
-                            if (contourArea >= minContourArea) {
-                                MatOfPoint2f contour2f = new MatOfPoint2f(contour.toArray());
-                                double contourLength = Imgproc.arcLength(contour2f, true);
-                                MatOfPoint2f approxCurve = new MatOfPoint2f();
-                                Imgproc.approxPolyDP(contour2f, approxCurve, 0.02 * contourLength, true);
-
-                                // Calcular el promedio de tono, saturación y brillo en la región del contorno
-                                Mat roi = new Mat(hsvImage, Imgproc.boundingRect(contour));
-                                Scalar contourHsvMean = Core.mean(roi);
-                                double hueValue = contourHsvMean.val[0];
-                                double saturationValue = contourHsvMean.val[1];
-                                double value = contourHsvMean.val[2];
-
-                                // Distinguir entre azul y celeste en función de los valores de tono, saturación y brillo
-                                if (hueValue >= blueHueMin && hueValue <= blueHueMax
-                                        && saturationValue >= saturationMin && saturationValue <= saturationMax
-                                        && value >= valueMin && value <= valueMax) {
-                                    blueContours.add(contour);
-                                } else if (hueValue >= cyanHueMin && hueValue <= cyanHueMax
-                                        && saturationValue >= saturationMin && saturationValue <= saturationMax
-                                        && value >= valueMin && value <= valueMax) {
-                                    cyanContours.add(contour);
-                                }
-                            }
-                        }
-// Dibujar los contornos filtrados en la imagen original
-                        Imgproc.drawContours(image, blueContours, -1, new Scalar(0, 0, 255), 2); // Dibujar en rojo para azules
-                        Imgproc.drawContours(image, cyanContours, -1, new Scalar(255, 0, 0), 2); // Dibujar en azul para celestes
-
-// Agregar la cantidad de piezas azules y celestes como texto en la imagen
-                        String blueText = "Azules: " + blueContours.size();
-                        String cyanText = "Celestes: " + cyanContours.size();
-                        Imgproc.putText(image, blueText, new Point(10, 30), Imgproc.FONT_HERSHEY_SIMPLEX, 1, new Scalar(0, 0, 255), 2);
-                        Imgproc.putText(image, cyanText, new Point(10, 60), Imgproc.FONT_HERSHEY_SIMPLEX, 1, new Scalar(255, 0, 0), 2);
-
-// Mostrar la imagen con los contornos y la información en una nueva ventana
-                        MatOfByte contourBuffer = new MatOfByte();
-                        Imgcodecs.imencode(".jpg", image, contourBuffer);
-                        ImageIcon contourIcon = new ImageIcon(contourBuffer.toArray());
-                        JLabel contourLabel = new JLabel(contourIcon);
-
-                        JFrame contourFrame = new JFrame("Contornos de Piezas Azules y Celestes: " + file.getName());
-                        contourFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-                        contourFrame.add(contourLabel);
-                        contourFrame.pack();
-                        contourFrame.setVisible(true);
-
-                    } else {
-                        System.out.println("No se pudo cargar la imagen: " + file.getName());
-                    }
-                } catch (Exception e) {
-                    System.out.println("Error al cargar la imagen: " + file.getName());
-                    e.printStackTrace();
-                }
+            if (file.isFile() && count < 5) {
+                processSingleImage(file);
                 count++;
             }
         }
     }
+
+    private static void processSingleImage(File file) {
+        try {
+            Mat image = Imgcodecs.imread(file.getAbsolutePath());
+
+            if (!image.empty()) {
+                Imgproc.resize(image, image, new Size(640, 480));
+                Mat hsvImage = new Mat();
+                Imgproc.cvtColor(image, hsvImage, Imgproc.COLOR_BGR2HSV);
+
+                Mat combinedMask = createCombinedMask(hsvImage);
+                List<MatOfPoint> contours = findContours(combinedMask);
+                List<MatOfPoint> blueContours = new ArrayList<>();
+                List<MatOfPoint> cyanContours = new ArrayList<>();
+
+                for (MatOfPoint contour : contours) {
+                    double contourArea = Imgproc.contourArea(contour);
+
+                    if (contourArea >= 100) {
+                        classifyContours(contour, blueContours, cyanContours, hsvImage);
+                    }
+                }
+
+                drawContoursAndText(image, blueContours, cyanContours, file.getName());
+            } else {
+                System.out.println("No se pudo cargar la imagen: " + file.getName());
+            }
+        } catch (Exception e) {
+            System.out.println("Error al cargar la imagen: " + file.getName());
+            e.printStackTrace();
+        }
+    }
+
+    private static Mat createCombinedMask(Mat hsvImage) {
+        Scalar blueLowerBound = new Scalar(100, 100, 50);
+        Scalar blueUpperBound = new Scalar(130, 255, 255);
+        Scalar cyanLowerBound = new Scalar(80, 100, 50);
+        Scalar cyanUpperBound = new Scalar(100, 255, 255);
+
+        Mat blueMask = new Mat();
+        Core.inRange(hsvImage, blueLowerBound, blueUpperBound, blueMask);
+
+        Mat cyanMask = new Mat();
+        Core.inRange(hsvImage, cyanLowerBound, cyanUpperBound, cyanMask);
+
+        Mat combinedMask = new Mat();
+        Core.add(blueMask, cyanMask, combinedMask);
+
+        return combinedMask;
+    }
+
+    private static List<MatOfPoint> findContours(Mat mask) {
+        List<MatOfPoint> contours = new ArrayList<>();
+        Mat hierarchy = new Mat();
+        Imgproc.findContours(mask, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+        return contours;
+    }
+
+    private static void classifyContours(MatOfPoint contour, List<MatOfPoint> blueContours, List<MatOfPoint> cyanContours, Mat hsvImage) {
+        double contourArea = Imgproc.contourArea(contour);
+
+        if (contourArea >= 100) {
+            MatOfPoint2f contour2f = new MatOfPoint2f(contour.toArray());
+            double contourLength = Imgproc.arcLength(contour2f, true);
+            MatOfPoint2f approxCurve = new MatOfPoint2f();
+            Imgproc.approxPolyDP(contour2f, approxCurve, 0.02 * contourLength, true);
+
+            Mat roi = new Mat(hsvImage, Imgproc.boundingRect(contour));
+            Scalar contourHsvMean = Core.mean(roi);
+            double hueValue = contourHsvMean.val[0];
+            double saturationValue = contourHsvMean.val[1];
+            double value = contourHsvMean.val[2];
+
+            if (hueValue >= blueHueMin && hueValue <= blueHueMax
+                    && saturationValue >= saturationMin && saturationValue <= saturationMax
+                    && value >= valueMin && value <= valueMax) {
+                blueContours.add(contour);
+            } else if (hueValue >= cyanHueMin && hueValue <= cyanHueMax
+                    && saturationValue >= saturationMin && saturationValue <= saturationMax
+                    && value >= valueMin && value <= valueMax) {
+                cyanContours.add(contour);
+            }
+        }
+    }
+
+    private static void drawContoursAndText(Mat image, List<MatOfPoint> blueContours, List<MatOfPoint> cyanContours, String fileName) {
+        Imgproc.drawContours(image, blueContours, -1, new Scalar(0, 0, 255), 2); // Draw in red for blues
+        Imgproc.drawContours(image, cyanContours, -1, new Scalar(255, 0, 0), 2); // Draw in blue for cyans
+
+        String blueText = "Azules: " + blueContours.size();
+        String cyanText = "Celestes: " + cyanContours.size();
+        Imgproc.putText(image, blueText, new Point(10, 30), Imgproc.FONT_HERSHEY_SIMPLEX, 1, new Scalar(0, 0, 255), 2);
+        Imgproc.putText(image, cyanText, new Point(10, 60), Imgproc.FONT_HERSHEY_SIMPLEX, 1, new Scalar(255, 0, 0), 2);
+
+        MatOfByte contourBuffer = new MatOfByte();
+        Imgcodecs.imencode(".jpg", image, contourBuffer);
+        ImageIcon contourIcon = new ImageIcon(contourBuffer.toArray());
+        JLabel contourLabel = new JLabel(contourIcon);
+
+        JFrame contourFrame = new JFrame("Contornos de Piezas Azules y Celestes: " + fileName);
+        contourFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        contourFrame.add(contourLabel);
+        contourFrame.pack();
+        contourFrame.setVisible(true);
+    }
+
 }
