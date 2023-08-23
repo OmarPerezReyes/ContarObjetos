b507c3446ccf7d6d875a070a09e5775316738e4b
Omar Alejandro Pérez Reyes <2130073@upv.edu.mx>
2023-08-23 00:51:56 -0600

    Agregar comentarios descriptivos al código

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    // Valores para segmentar los colores en el espacio HSV
+    // Definición de umbrales de color para segmentación en el espacio HSV
     private static final double blueHueMin = 100;
     private static final double blueHueMax = 130;
     private static final double cyanHueMin = 80;
@@ -27,24 +27,32 @@ public class Main {
     private static final double valueMax = 255;
 
     public static void main(String[] args) {
-
+        // Carga la biblioteca OpenCV
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+
+        // Ruta de la carpeta que contiene las imágenes
         String folderPath = "C:\\Users\\perez\\IdeaProjects\\ContarObjetos\\assets\\photos";
+
+        // Procesa las imágenes en la carpeta
         processImages(folderPath);
     }
 
+    // Método para procesar imágenes en una carpeta
     static void processImages(String folderPath) {
-
+        // Obtiene la carpeta especificada
         File folder = new File(folderPath);
 
+        // Verifica si la carpeta existe y es válida
         if (!folder.exists() || !folder.isDirectory()) {
             System.out.println("La carpeta no existe o no es una carpeta válida.");
             return;
         }
 
+        // Obtiene la lista de archivos en la carpeta
         File[] files = folder.listFiles();
         int count = 0;
 
+        // Itera sobre los archivos y procesa las primeras 5 imágenes (puedes ajustar esto)
         for (File file : files) {
             if (file.isFile() && count < 5) { //files.length
                 processSingleImage(file);
@@ -52,55 +60,28 @@ public class Main {
             }
         }
     }
-    void processSingleImage(Mat image) {
-        try {
-            if (!image.empty()) {
-                Imgproc.resize(image, image, new Size(640, 480));
-                Mat hsvImage = new Mat();
-                Imgproc.cvtColor(image, hsvImage, Imgproc.COLOR_BGR2HSV);
-
-                Mat combinedMask = createCombinedMask(hsvImage);
-                List<MatOfPoint> contours = findContours(combinedMask);
-                List<MatOfPoint> blueContours = new ArrayList<>();
-                List<MatOfPoint> cyanContours = new ArrayList<>();
-                List<MatOfPoint> orangeContours = new ArrayList<>();
-                List<MatOfPoint> redContours = new ArrayList<>();
-                List<MatOfPoint> greenContours = new ArrayList<>();
-                List<MatOfPoint> yellowContours = new ArrayList<>();
-
-                for (MatOfPoint contour : contours) {
-                    double contourArea = Imgproc.contourArea(contour);
-
-                    if (contourArea >= 100) {
-                        classifyContours(contour, blueContours, cyanContours, orangeContours, redContours,
-                                greenContours, yellowContours, hsvImage);
-                    }
-                }
-
-                drawContoursAndText(image, blueContours, cyanContours, orangeContours, redContours,
-                        greenContours, yellowContours, image.toString());
-            } else {
-                System.out.println("No se pudo cargar la imagen: " + image.toString());
-            }
-        } catch (Exception e) {
-            System.out.println("Error al cargar la imagen: " + image.toString());
-            e.printStackTrace();
-        }
-    }
-
-
 
         private static void processSingleImage(File file) {
         try {
+            // Lee la imagen usando OpenCV
             Mat image = Imgcodecs.imread(file.getAbsolutePath());
 
+            // Verifica si la imagen se cargó correctamente
             if (!image.empty()) {
+                // Redimensiona la imagen a un tamaño específico
                 Imgproc.resize(image, image, new Size(640, 480));
+
+                // Convierte la imagen a espacio de color HSV
                 Mat hsvImage = new Mat();
                 Imgproc.cvtColor(image, hsvImage, Imgproc.COLOR_BGR2HSV);
 
+                // Crea la máscara combinada usando los umbrales de color
                 Mat combinedMask = createCombinedMask(hsvImage);
+
+                // Encuentra los contornos en la máscara combinada
                 List<MatOfPoint> contours = findContours(combinedMask);
+
+                // Inicializa listas para almacenar contornos de diferentes colores
                 List<MatOfPoint> blueContours = new ArrayList<>();
                 List<MatOfPoint> cyanContours = new ArrayList<>();
                 List<MatOfPoint> orangeContours = new ArrayList<>();
@@ -108,6 +89,7 @@ public class Main {
                 List<MatOfPoint> greenContours = new ArrayList<>();
                 List<MatOfPoint> yellowContours = new ArrayList<>();
 
+                // Clasifica los contornos en las listas de contornos de diferentes colores
                 for (MatOfPoint contour : contours) {
                     double contourArea = Imgproc.contourArea(contour);
 
@@ -117,6 +99,7 @@ public class Main {
                     }
                 }
 
+                // Dibuja contornos y muestra información en una ventana emergente
                 drawContoursAndText(image, blueContours, cyanContours, orangeContours, redContours,
                         greenContours, yellowContours, file.getName());
             } else {
@@ -128,7 +111,9 @@ public class Main {
         }
     }
 
+    // Método para crear la máscara combinada utilizando umbrales de color
     private static Mat createCombinedMask(Mat hsvImage) {
+        // Definición de umbrales de color para diferentes colores
         Scalar blueLowerBound = new Scalar(100, 100, 50);
         Scalar blueUpperBound = new Scalar(130, 255, 255);
         Scalar cyanLowerBound = new Scalar(80, 100, 50);
@@ -144,6 +129,7 @@ public class Main {
         Scalar yellowLowerBound = new Scalar(25, 100, 50);
         Scalar yellowUpperBound = new Scalar(35, 255, 255);
 
+        // Creación de máscaras individuales para cada color
         Mat blueMask = new Mat();
         Core.inRange(hsvImage, blueLowerBound, blueUpperBound, blueMask);
 
@@ -166,6 +152,7 @@ public class Main {
         Mat yellowMask = new Mat();
         Core.inRange(hsvImage, yellowLowerBound, yellowUpperBound, yellowMask);
 
+        // Combinación de máscaras individuales en una máscara combinada
         Mat combinedMask = new Mat();
         Core.add(blueMask, cyanMask, combinedMask);
         Core.add(combinedMask, orangeMask, combinedMask);
@@ -176,19 +163,24 @@ public class Main {
         return combinedMask;
     }
 
+    // Método para encontrar contornos en una máscara
     private static List<MatOfPoint> findContours(Mat mask) {
         List<MatOfPoint> contours = new ArrayList<>();
+        // Encuentra los contornos en la máscara
         Mat hierarchy = new Mat();
         Imgproc.findContours(mask, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
         return contours;
     }
 
+    // Método para clasificar contornos en diferentes listas de contornos basados en el color
     private static void classifyContours(MatOfPoint contour, List<MatOfPoint> blueContours, List<MatOfPoint> cyanContours,
                                          List<MatOfPoint> orangeContours, List<MatOfPoint> redContours,
                                          List<MatOfPoint> greenContours, List<MatOfPoint> yellowContours,
                                          Mat hsvImage) {
+        // Calcula características del contorno y del color en el área del contorno
         double contourArea = Imgproc.contourArea(contour);
 
+        // Clasifica el contorno en la lista de contornos adecuada según su color
         if (contourArea >= 100) {
             MatOfPoint2f contour2f = new MatOfPoint2f(contour.toArray());
             double contourLength = Imgproc.arcLength(contour2f, true);
@@ -210,7 +202,7 @@ public class Main {
                     redContours.add(contour);
                 } else if (hueValue >= 0 && hueValue <= 10) {
                     redContours.add(contour);
-                }
+                }         // (Clasificación para otros colores)
             } else if (hueValue >= greenHueMin && hueValue <= greenHueMax
                     && saturationValue >= saturationMin && saturationValue <= saturationMax
                     && value >= valueMin && value <= valueMax) {
@@ -236,10 +228,12 @@ public class Main {
     }
 
 
+    // Método para dibujar contornos y mostrar información en una ventana emergente
     private static void drawContoursAndText(Mat image, List<MatOfPoint> blueContours, List<MatOfPoint> cyanContours,
                                             List<MatOfPoint> orangeContours, List<MatOfPoint> redContours,
                                             List<MatOfPoint> greenContours, List<MatOfPoint> yellowContours,
                                             String fileName) {
+        // Dibuja los contornos en la imagen
         Imgproc.drawContours(image, blueContours, -1, new Scalar(255, 0, 0), 2);  // Azul
         Imgproc.drawContours(image, cyanContours, -1, new Scalar(255, 255, 0), 2); // Cyan
         Imgproc.drawContours(image, orangeContours, -1, new Scalar(0, 165, 255), 2); // Naranja
@@ -247,6 +241,7 @@ public class Main {
         Imgproc.drawContours(image, greenContours, -1, new Scalar(0, 255, 0), 2);   // Verde
         Imgproc.drawContours(image, yellowContours, -1, new Scalar(0 , 255, 255), 2); // Amarillo
 
+        // Agrega información de conteo de objetos en la imagen
         String blueText = "Azules: " + blueContours.size();
         String cyanText = "Celestes: " + cyanContours.size();
         String orangeText = "Naranjas: " + orangeContours.size();
@@ -254,6 +249,7 @@ public class Main {
         String greenText = "Verdes: " + greenContours.size();
         String yellowText = "Amarillos: " + yellowContours.size();
 
+        // Agrega el texto en la imagen
         Imgproc.putText(image, blueText, new Point(10, 30), Imgproc.FONT_HERSHEY_SIMPLEX, 1, new Scalar(255, 0, 0), 2);
         Imgproc.putText(image, cyanText, new Point(10, 60), Imgproc.FONT_HERSHEY_SIMPLEX, 1, new Scalar(255, 255, 0), 2);
         Imgproc.putText(image, orangeText, new Point(10, 90), Imgproc.FONT_HERSHEY_SIMPLEX, 1, new Scalar(0, 165, 255), 2);
@@ -261,6 +257,7 @@ public class Main {
         Imgproc.putText(image, greenText, new Point(10, 150), Imgproc.FONT_HERSHEY_SIMPLEX, 1, new Scalar(0, 255, 0), 2);
         Imgproc.putText(image, yellowText, new Point(10, 180), Imgproc.FONT_HERSHEY_SIMPLEX, 1, new Scalar(0, 255, 255), 2);
 
+        // Muestra la imagen en una ventana emergente
         MatOfByte contourBuffer = new MatOfByte();
         Imgcodecs.imencode(".jpg", image, contourBuffer);
         ImageIcon contourIcon = new ImageIcon(contourBuffer.toArray());
